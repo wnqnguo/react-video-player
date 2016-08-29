a82244ac81ad6d14c05650383573f644b140c251
Wenqian Guo <wnqnguo@gmail.com>
2016-08-29 12:36:18 -0700

    added you tube api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search'
 const API_KEY ='AIzaSyDIGZqJQwaGSs6p0BOuQ6UBBupzlzTmgxw';
 //Create a new component. 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+})
 const App = () => {
 	return (
 		<div>
