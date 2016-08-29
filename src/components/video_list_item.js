848aff591f613cea227e5f1e65ebc0b0085d5026
Wenqian Guo <wnqnguo@gmail.com>
2016-08-29 14:50:56 -0700

    added styling for video_list_item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,5 +1,17 @@
 import React from 'react';
-const VideoListItem = (props) => {
-	return <li> Video</li>
+const VideoListItem = ({video}) => {
+	console.log(video);
+	return (
+		<li className = "list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+				<img className="media-object" />
+				</div>
+			</div>
+			<div className="media-body">
+				<div className="media-heading"></div>
+			</div>
+		</li>
+	);
 };
 export default VideoListItem;
